835794db33ff9e62e168a66acc2e7aaf779a30fb
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-10-08 19:20:50 +0100

    fixed

--- main.lua
+++ main.lua
@@ -1,23 +1,12 @@
 repeat task.wait() until game:IsLoaded()
 
-local mins = getgenv().Xminutes
-local kickinstead = getgenv().KickInstead
-local hideaw = getgenv().HideAppleWareUI
-local t = tick()
-
-if (hideaw) then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/railme3750914/autorejoiner/main/HideAppleWare"))()
-end
-
-queue_on_teleport("getgenv().Xminutes="..mins..";getgenv().KickInstead="..tostring(kickinstead)..";getgenv().HideAppleWareUI="..tostring(hideaw)..[[;loadstring(game:HttpGet("https://raw.githubusercontent.com/railme3750914/autorejoiner/main/hideappleware.lua"))()]])
-
-while task.wait(1) do
-   if (tick() - t > 60 * mins then
-      if (kickinstead) then
-          game.Players.LocalPlayer:Kick("Kicking You...")
-      else
-          game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
-      end
-   end
-end
-game.StarterGui:SetCore("SendNotification", {Title="Activated"Text="Auto Rejoin is now working with your settings!"Duration=5})
+queue_on_teleport("loadstring(game:HttpGet(\"https://raw.githubusercontent.com/railme37509124/autorejoiner/main/main.lua\"))()")
+Mins = 0.3 --temporarily 0.3 for testing
+loadstring(game:HttpGet("https://raw.githubusercontent.com/railme37509124/autorejoiner/main/HideAppleWare"))()
+game.StarterGui:SetCore("SendNotification", {
+   Title = "Active",
+   Text = "Successfully enabled!",
+   Duration = 5
+})
+wait(60*Mins)
+game.Players.LocalPlayer:Kick("waiting for atlas to rejoin")
