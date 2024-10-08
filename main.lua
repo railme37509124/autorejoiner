26028efc94731a013c08f4e439a0d74db5e83ea8
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-10-08 18:44:24 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,10 +6,10 @@ local hideaw = getgenv().HideAppleWareUI
 local t = tick()
 
 if (hideaw) then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/railme3750914/autorejoiner/main/hideappleware.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/railme3750914/autorejoiner/main/HideAppleWare"))()
 end
 
-queue_on_teleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/railme3750914/autorejoiner/main/hideappleware.lua"))()]])
+queue_on_teleport("getgenv().Xminutes="..mins..";getgenv().KickInstead="..kickinstead..";getgenv().HideAppleWareUI="..tostring(hideaw)..[[;loadstring(game:HttpGet("https://raw.githubusercontent.com/railme3750914/autorejoiner/main/hideappleware.lua"))()]])
 
 while task.wait(1) do
    if (tick() - t > 60 * mins then
