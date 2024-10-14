89c297532b126f82623a52780c8ab2cae44587b4
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-10-14 01:17:37 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,11 +3,12 @@
 repeat task.wait() until game:IsLoaded()
 
 queue_on_teleport("loadstring(game:HttpGet(\"https://raw.githubusercontent.com/railme37509124/autorejoiner/main/main.lua\"))()")
-Mins = 135 -- 2 hrs 15 mins
+Mins = 75 -- 1 hrs 15 mins
 loadstring(game:HttpGet("https://raw.githubusercontent.com/railme37509124/autorejoiner/main/HideAppleWare"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/railme37509124/autorejoiner/main/HideAtlas"))()
 game.StarterGui:SetCore("SendNotification", {
    Title = "Active",
-   Text = "Successfully enabled! (per "..Mins.." minutes)",
+   Text = "Successfully enabled! (per "..Mins.." minutes) | 000",
    Duration = 5
 })
 wait(60*Mins)
